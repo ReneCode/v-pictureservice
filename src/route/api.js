3c7f9c1e55149ef7c1a9c9e38619b85e3e064ddb
rene_imac <HarryMillerMail@googlemail.com>
2017-07-16 18:20:20 +0200

    refactor: svg2png removed

--- src/route/api.js
+++ src/route/api.js
@@ -4,7 +4,6 @@ var express = require('express');
 var router = express.Router();
 
 let blobStorage = require('../blobstorage/blobstorage');
-let SvgAsPng = require('./svg-as-png');
 
 const tenantId = '0a7b63de-9e54-4d7d-a3b0-d15a2aef8679';
 
@@ -29,41 +28,6 @@ function getSvg(req, res) {
     })
 }
 
-// function toDataURL(blob) {
-// 	new Promise((resolve, reject) => {
-// 		const reader = new FileReader()
-// 		reader.onloadend = () => resolve(reader.result)
-// 		reader.onerror = reject
-// 		reader.readAsDataURL(blob)
-// 	});
-// }
-
-function getSvgAsPng(req, res) {
-  const projectId = req.params.projectId;
-  const fileName = req.params.fileName;
-  const query = req.query;
-
-  const containerName = blobStorage.getContainerName(tenantId);
-  const key = blobStorage.getKey(projectId, fileName);
-  blobStorage.getBlob(containerName, key)
-    .then((svg) => {
-      const svgAsPng = new SvgAsPng();
-      return svgAsPng.convert(svg);
-    })
-    .then((png) => {
-      res.writeHead(200,
-        {
-          'Content-Type': 'image/png'
-        });
-      res.end(png, 'binary');
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500);
-    })
-}
-
-
 function getImage(req, res) {
   const projectId = req.params.projectId;
   const imageName = req.params.imageName.toLowerCase();
@@ -71,8 +35,6 @@ function getImage(req, res) {
 
   const containerName = blobStorage.getContainerName(tenantId);
   const key = blobStorage.getKey(projectId, imageName);
-  console.log("KEY:", key)
-  let stream = fs.createWriteStream("tmp.png")
   blobStorage.getBlobToStream(containerName, key, res)
     .then(() => {
       res.end();
@@ -84,7 +46,6 @@ function getImage(req, res) {
 }
 
 router.get("/svgs/:projectId/:fileName", getSvg);
-router.get("/svgs/:projectId/:fileName/png", getSvgAsPng);
 
 router.get("/images/:projectId/:imageName", getImage);
 
